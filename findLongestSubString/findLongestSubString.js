ec8f3ed73b07237f3b94842fe5310773ec57df99
yazansaadeh <yazansaada0@gmail.com>
2024-12-09 20:20:00 +0300

    solve recursion problem

--- findLongestSubString/findLongestSubString.js
+++ findLongestSubString/findLongestSubString.js
@@ -1,1 +1,20 @@
-// function findLongestSubString(str) {}
+function findLongestSubString(str) {
+  let object = {};
+  let maxLen = -Infinity;
+  let start = 0;
+  let end = 0;
+  while (end < str.length) {
+    let char = str[end];
+    if (!object[char]) {
+      object[char] = 1;
+      end++;
+      maxLen = Math.max(maxLen, end - start);
+    } else {
+      object[str[start]]--;
+      start++;
+    }
+  }
+  return maxLen;
+}
+findLongestSubString("thisisawesome");
+//{t:0 , h:0 , i:0 , s:1 , a:1 , w:1 , e:1 ,o:1, m:1 ,  }
